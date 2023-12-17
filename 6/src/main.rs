bb9dd26840f7d43d834827384dd9f188b97b1933
Sean Frankruyter <s.frankruyter@gmail.com>
2023-12-16 20:44:30 -0500

    pt 2

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -63,13 +63,43 @@ fn part_1 (input: &str) -> i32 {
     answer
 }
 
-fn calc_distance (time: &i32, hold_ms: &usize) -> i32 {
-    *hold_ms as i32 * (time - *hold_ms as i32)
+fn part_2 (input: &str) -> i64 {
+    let re_line = Regex::new(r"\d+").unwrap();
+    
+    // get the first line of the file, split on : and then use the regex to get the number i do not want arrays
+    let (times, distances_to_beat) = input
+        .split('\n')
+        .map(|line| {
+            re_line
+                .find_iter(line.split_once(':').unwrap().1)
+                .map(|m| m.as_str().parse().unwrap())
+                .collect::<Vec<i64>>()
+        })
+        .collect_tuple()
+        .unwrap();
+
+    let time: i64 = times[0];
+    let goal: i64 = distances_to_beat[0];
+
+
+     let ways_to_beat_record = (0..time)
+        .enumerate()
+        .filter(|(hold_ms, _)| {
+            let distance = calc_distance(&time, &hold_ms);
+
+            if distance > goal {
+                // println!("Hold: {}", hold_ms);
+                return true;
+            }
+
+            false
+        })
+        .collect::<Vec<(usize, i64)>>()
+        .len();
+
+    ways_to_beat_record as i64
 }
 
-#[derive(Debug)]
-struct Race {
-    time: i32,
-    distance: i32,
-    margin: i32,
+fn calc_distance (time: &i64, hold_ms: &usize) -> i64 {
+    *hold_ms as i64 * (time - *hold_ms as i64)
 }
